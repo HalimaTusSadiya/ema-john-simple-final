eb1e14ddf6be049c70bac3cf820594292d83d20d
Halima sadia <92320260+Halima-tussadia@users.noreply.github.com>
2023-02-10 20:17:41 +0600

    routes updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter , Routes, Route, Link } from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+          <Routes>
+              <Route  path="/shop" element={<Shop/>} />
+              <Route  path="/review" element={<Review/>} />
+              <Route  path="/manage" element={<Inventory/>} />
+              <Route  exact path="/" element={<Shop/>} />
+              <Route  path="*" element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
